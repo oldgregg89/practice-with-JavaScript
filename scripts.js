29e2b33862c5f5106325df409f85c9f4501d7ffc
Ian Gregg <iangregg188@gmail.com>
2020-10-12 17:03:16 -0700

    added in button functionality for the javaScript.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+const changeBackgroundGreen = function() {
+  document.getElementById("laLaLand").classList.add("greenChange")
+}
+
+const changeBackgroundRed = function() {
+  document.getElementById("laLaLand").classList.add("redChange")
+}
+
+const changeBackgroundBlack = function() {
+  document.getElementById("laLaLand").classList.add("blackChange")
+}
